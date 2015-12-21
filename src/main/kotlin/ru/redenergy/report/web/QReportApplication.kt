d3879540f0dd2917aefa607b0a3d1a125db3067e
RedEnergy <forwot163@gmail.com>
2015-12-21 21:40:38 +0400

    QReportApplication: added database connection, tickets dao and routes for tickets list

--- src/main/kotlin/ru/redenergy/report/web/QReportApplication.kt
+++ src/main/kotlin/ru/redenergy/report/web/QReportApplication.kt
@@ -1,12 +1,28 @@
 package ru.redenergy.report.web
 
+import com.j256.ormlite.dao.Dao
+import com.j256.ormlite.dao.DaoManager
+import com.j256.ormlite.field.DataPersisterManager
+import com.j256.ormlite.jdbc.JdbcConnectionSource
 import de.neuland.jade4j.JadeConfiguration
-import spark.ModelAndView
+import ru.redenergy.report.web.entities.Ticket
+import ru.redenergy.report.web.orm.JsonPersister
+import ru.redenergy.report.web.routes.ReportsListRoute
+import ru.redenergy.report.web.routes.ViewReportRoute
 import spark.Spark
 import spark.template.jade.JadeTemplateEngine
 import spark.template.jade.loader.SparkClasspathTemplateLoader
+import java.util.*
 
 class QReportApplication {
+
+    init{
+        DataPersisterManager.registerDataPersisters(JsonPersister.getSingleton())
+    }
+
+    var connectionSource = JdbcConnectionSource("jdbc:mysql://localhost:3306/qreport", "root", "mysql") //TODO: make database configurable
+
+    var ticketDao = DaoManager.createDao<Dao<Ticket, UUID>, Ticket>(connectionSource, Ticket::class.java)
 
     val jadeEngine = JadeTemplateEngine(JadeConfiguration().apply {
         templateLoader = SparkClasspathTemplateLoader("templates")
@@ -18,9 +34,8 @@ class QReportApplication {
     }
 
     fun registerRoutes(){
-        Spark.get("/", { request, response ->
-            ModelAndView(mapOf<Any, Any>(), "index")
-        }, jadeEngine)
+        Spark.get("/admin/reports", ReportsListRoute(this), jadeEngine)
+        Spark.get("/admin/reports/:id", ViewReportRoute(this), jadeEngine)
     }
 
 }
