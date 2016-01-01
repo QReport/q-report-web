4dd526181b319986be219a0d17feddb2e7a177fa
RedEnergy <forwot163@gmail.com>
2016-01-02 00:31:18 +0400

    Added config for app

--- src/main/kotlin/ru/redenergy/report/web/Starter.kt
+++ src/main/kotlin/ru/redenergy/report/web/Starter.kt
@@ -1,10 +1,13 @@
 package ru.redenergy.report.web
 
+import ru.redenergy.report.web.config.StandaloneConfig
+
 object Starter {
-    //Instance of application
-    val app: QReportApplication = QReportApplication()
+    lateinit var app: QReportApplication
 }
 
 fun main(args: Array<String>) {
+    val config = StandaloneConfig("jdbc:mysql://localhost:3306/qreport", "root", "mysql")
+    Starter.app = QReportApplication(config)
     Starter.app.start(args)
 }
