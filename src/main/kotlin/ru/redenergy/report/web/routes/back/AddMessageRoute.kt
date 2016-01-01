d299ff8c4580f1ef3cd21f93848ea14fd2b135c7
RedEnergy <forwot163@gmail.com>
2016-01-02 00:31:50 +0400

    Implemented ability to add message

--- src/main/kotlin/ru/redenergy/report/web/routes/back/AddMessageRoute.kt
+++ src/main/kotlin/ru/redenergy/report/web/routes/back/AddMessageRoute.kt
@@ -2,6 +2,7 @@ package ru.redenergy.report.web.routes.back
 
 import ru.redenergy.report.web.QReportApplication
 import ru.redenergy.report.web.entities.TicketMessage
+import ru.redenergy.report.web.response.StatusResponse
 import spark.Request
 import spark.Response
 import spark.Route
@@ -12,8 +13,10 @@ class AddMessageRoute(val app: QReportApplication): Route {
     override fun handle(request: Request, response: Response): Any {
         val uid = UUID.fromString(request.params("id"))
         val ticket = app.ticketDao.queryForId(uid)
-        val text = request.params("text")
-        return ""
+        val text = request.queryParams("text")
+        ticket.messages.add(TicketMessage("Unknown Sender", text))
+        app.ticketDao.update(ticket)
+        return StatusResponse(true, null)
     }
 
 }
