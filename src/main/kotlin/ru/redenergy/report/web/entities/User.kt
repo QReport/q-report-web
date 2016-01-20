f2900fd7f26679a160ae4dfbefbba30d676cb928
RedEnergy <forwot163@gmail.com>
2016-01-20 22:14:13 +0400

    password and access token marked as transient to prevent leak

--- src/main/kotlin/ru/redenergy/report/web/entities/User.kt
+++ src/main/kotlin/ru/redenergy/report/web/entities/User.kt
@@ -5,6 +5,6 @@ import java.util.*
 import com.j256.ormlite.field.DatabaseField as db
 
 @DatabaseTable(tableName = "users")
-data class User(@db(id = true) var uid: UUID, @db var username: String, @db var passwordHash: String, @db var accessToken: String) {
+data class User(@db(id = true) var uid: UUID, @db var username: String, @Transient @db var passwordHash: String, @Transient @db var accessToken: String) {
     constructor(): this(UUID.randomUUID(), "", "", "")
 }
