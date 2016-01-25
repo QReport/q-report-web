2fcb135f3292850c91d94c2536f059e1872d937b
RedEnergy <forwot163@gmail.com>
2016-01-25 21:40:54 +0400

    Added field to indicate if user has full access to servers

--- src/main/kotlin/ru/redenergy/report/web/entities/User.kt
+++ src/main/kotlin/ru/redenergy/report/web/entities/User.kt
@@ -1,10 +1,34 @@
 package ru.redenergy.report.web.entities
 
 import com.j256.ormlite.table.DatabaseTable
+import ru.redenergy.report.web.orm.JsonPersister
 import java.util.*
-import com.j256.ormlite.field.DatabaseField as db
+import com.j256.ormlite.field.DatabaseField
 
 @DatabaseTable(tableName = "users")
-data class User(@db(id = true) var uid: UUID, @db var username: String, @Transient @db var passwordHash: String, @Transient @db var accessToken: String) {
-    constructor(): this(UUID.randomUUID(), "", "", "")
+
+data class User(@DatabaseField(id = true)
+                var uid: UUID,
+
+                @DatabaseField
+                var username: String,
+
+                @DatabaseField
+                var editUsers: Boolean,
+
+                @DatabaseField
+                var fullServerAccess: Boolean,
+
+                @DatabaseField(persisterClass = JsonPersister::class)
+                val serverPermission:  List<Permission>,
+
+                @Transient @DatabaseField
+                var passwordHash: String,
+
+                @Transient @DatabaseField
+                var accessToken: String) {
+
+
+
+    constructor(): this(UUID.randomUUID(), "", false, false, arrayListOf<Permission>(), "", "")
 }
