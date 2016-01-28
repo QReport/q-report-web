e02870ec0ba83f15c25f97055ad88bb60e880907
RedEnergy <forwot163@gmail.com>
2016-01-28 21:49:53 +0400

    StatsRoute code optimized for last version of commons

--- src/main/kotlin/ru/redenergy/report/web/routes/StatsRoute.kt
+++ src/main/kotlin/ru/redenergy/report/web/routes/StatsRoute.kt
@@ -2,8 +2,9 @@ package ru.redenergy.report.web.routes
 
 import com.google.gson.Gson
 import com.google.gson.JsonObject
-import ru.redenergy.report.common.TicketReason
-import ru.redenergy.report.common.entity.Ticket
+import ru.redenergy.report.common.entity.activeUsers
+import ru.redenergy.report.common.entity.averageResponseTime
+import ru.redenergy.report.common.entity.countReasons
 import ru.redenergy.report.web.QReportApplication
 import ru.redenergy.report.web.response.StatusResponse
 import spark.Request
@@ -15,29 +16,11 @@ public class StatsRoute(val app: QReportApplication): Route{
     override fun handle(request: Request, response: Response): Any? {
         val tickets = app.ticketDao.queryForAll()
         val result = JsonObject().apply {
-            add("tickets", Gson().toJsonTree(countTickets(tickets)))
-            add("users", Gson().toJsonTree(getActiveUsers(tickets)))
-            addProperty("average", calculateAverageResponseTime(tickets))
+            add("tickets", Gson().toJsonTree(tickets.countReasons()))
+            add("users", Gson().toJsonTree(tickets.activeUsers(5)))
+            addProperty("average", tickets.averageResponseTime())
         }
         return StatusResponse(true, result)
     }
-
-    fun countTickets(tickets: MutableList<Ticket>): Map<TicketReason, Int> =
-            mapOf(*TicketReason.values
-                    .map { r -> Pair(r, tickets.count { it.reason.equals(r) })}
-                    .toTypedArray())
-
-
-    fun getActiveUsers(tickets: MutableList<Ticket>): Map<String, Int> =
-            linkedMapOf(*tickets
-                    .map { it.sender }
-                    .map { Pair(it, tickets.count { t -> t.sender.equals(it) }) }
-                    .toTypedArray())
-
-    fun calculateAverageResponseTime(tickets: MutableList<Ticket>): Long =
-            tickets.filter { t -> t.messages.any { m -> m.sender != t.sender } }
-                    .map { it.messages.first { m -> m.sender != it.sender } .timestamp - it.messages[0].timestamp }
-                    .run { return sum() / count() }
-
 
 }
