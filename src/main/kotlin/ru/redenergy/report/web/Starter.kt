c0c7c181303d786f716cbb55512b1cad65ba871b
RedEnergy <forwot163@gmail.com>
2016-02-01 20:58:56 +0400

    Added command line config parser

--- src/main/kotlin/ru/redenergy/report/web/Starter.kt
+++ src/main/kotlin/ru/redenergy/report/web/Starter.kt
@@ -1,5 +1,6 @@
 package ru.redenergy.report.web
 
+import ru.redenergy.report.web.config.IAppConfig
 import ru.redenergy.report.web.config.StandaloneConfig
 
 object Starter {
@@ -7,7 +8,26 @@ object Starter {
 }
 
 fun main(args: Array<String>) {
-    val config = StandaloneConfig("jdbc:mysql://localhost:3306/qreport", "root", "mysql")
+    val config = parseConfigFromArguments(args)
     Starter.app = QReportApplication(config)
     Starter.app.start(args)
 }
+
+fun parseConfigFromArguments(args: Array<String>): IAppConfig{
+    var path = "jdbc:mysql://localhost:3306/qreport"
+    var login = "root"
+    var password = "password"
+    var port = 4567
+
+    for((i, s) in args.withIndex()){
+        when(s){
+            "-url" -> path = args[i + 1]
+            "-login" -> login = args[i + 1]
+            "-pass" -> password = args[i + 1]
+            "-port" -> port = args[i + 1].toInt()
+        }
+    }
+
+    return StandaloneConfig(path, login, password, port)
+}
+
--- src/main/kotlin/ru/redenergy/report/web/config/StandaloneConfig.kt
+++ src/main/kotlin/ru/redenergy/report/web/config/StandaloneConfig.kt
@@ -1,3 +1,3 @@
 package ru.redenergy.report.web.config
 
-class StandaloneConfig(override val databasePath: String, override val databaseLogin: String, override val databasePassword: String, override val port: Int): IAppConfig
+class StandaloneConfig(override val databasePath: String, override val databaseLogin: String, override val databasePassword: String, override val port: Int = 4567): IAppConfig
